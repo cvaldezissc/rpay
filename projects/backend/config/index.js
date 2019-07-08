c28034dff1561e511378dc43cf8f0b8d2a7d46a6
cvaldezissc <cvaldez.issc@gmail.com>
2019-07-08 01:46:55 -0500

    base configuration file added

--- projects/backend/config/index.js
+++ projects/backend/config/index.js
@@ -1,3 +1,18 @@
 module.exports = {
+    appConfig: {
+        port: 8000,
+        timeBetweenMaxNumberOfRequestsInMin: 15,
+        maximumRequestNumberAllowedPerTime: 10
+    },
+    persistanceConfig: {
+        mongodb: {
+            
+        },
+        redis:{
+            host: "127.0.0.1",
+            port: "6379",
+            password: ""
+        }
+    },
     jwtSecret: 'ANY_RANDOM_STRING'
 };
